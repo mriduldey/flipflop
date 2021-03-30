4879a5dd81a5cf09119c632e3c3f15dfd3d1e48b
unknown <mriduldey94@gmail.com>
2021-03-30 12:21:13 +0530

    Updates

---
+++ precache-manifest.75d716022d7ae21b4eb6d50a2616c177.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb8bf5790b05cb141cc8c7d899f5987c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fe59a15705739d410b82",
+    "url": "/static/css/main.1be28493.chunk.css"
+  },
+  {
+    "revision": "08b40ada0e79ccdd34ad",
+    "url": "/static/js/2.7ed7d2ed.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.7ed7d2ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe59a15705739d410b82",
+    "url": "/static/js/main.23825010.chunk.js"
+  },
+  {
+    "revision": "2c8dc3630f499af46148",
+    "url": "/static/js/runtime-main.d171d662.js"
+  },
+  {
+    "revision": "bd83c4a218f78907d018bdadb3920887",
+    "url": "/static/media/batman.bd83c4a2.png"
+  }
+]);
